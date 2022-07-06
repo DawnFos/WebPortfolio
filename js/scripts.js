003afd4c49a16dc5d120b4a7bd22c6e6731b829c
Dawn Fos <dawn.lee.fos@gmail.com>
2022-07-05 19:51:20 -0500

    first draft

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,25 @@ $(function() {
         }
     });
 });
+
+(function(){
+    emailjs.init("_Jhys9gG5a2LmUQMU");
+})();
+
+function sendmail() {
+    let fullName = document.getElementById("name").value;
+    let userEmail = document.getElementById("email").value;
+    let userNumber = document.getElementById("number").value;
+    let callCheck = document.getElementById("call").value;
+    let userMessage = document.getElementById("message").value;
+
+        var contactParams = {
+            from_name: fullName,
+            from_email: userEmail,
+            from_number: userNumber,
+            call_req: callCheck,
+            message: userMessage,
+        };
+
+        emailjs.send('service_4m5m5p5', 'template_4j1rd97', contactParams).then(function (res) {})
+}
